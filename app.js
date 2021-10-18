381b49dbf2d51d92f8e5e32582b72eb057104a2d
Muhammad Abuzar <abuzar9658@gmail.com>
2021-10-18 18:58:29 +0500

    connected with cx9 apis

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express")
-const app = express()
-const morgan = require('morgan')
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
 // const user = require('./users.json')
 // console.log(user.users[0].password)
-const cors  = require("cors")
+const cors = require("cors");
 
-app.use(cors())
-app.use(morgan())
-app.use(express.json())
-app.use(require('./routes/routes'))
+app.use(cors());
+// app.use(morgan())
+app.use(express.json());
+app.use(require("./routes/routes"));
 
-app.listen(5000, ()=>{
-    console.log("server started at port 5000")
-})
+app.listen(5000, () => {
+  console.log("server started at port 5000");
+});
