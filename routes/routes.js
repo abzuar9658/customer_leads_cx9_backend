ebd44c1b0aaecfcc385ec54535273c6ade247396
Muhammad Abuzar <72570410+abzuar9658@users.noreply.github.com>
2021-10-20 11:48:52 +0500

    refresh commit

--- routes/routes.js
+++ routes/routes.js
@@ -7,71 +7,72 @@ const axios = require("axios");
 // leads route
 
 //login route
-router.post("/api/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const result = await axios.post(
-      "https://cx9dev.contegris.com:8443/apis/auth",
-      {
-        username: username,
-        password: password,
-      }
-    );
-    res.status(200).json({
-      status: "ok",
-      data: {
-        token: result.data.data.token,
-      },
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
+router.post("/api/login", async(req, res) => {
+    try {
+        const { username, password } = req.body;
+        const result = await axios.post(
+            "https://cx9dev.contegris.com:8443/apis/auth", {
+                username,
+                password,
+            }
+        );
+        res.status(200).json({
+            status: "ok",
+            data: {
+                token: result.data.data.token,
+            },
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(404).json({
+            status: "fail",
+            message: error.message,
+        });
+    }
 });
 
 //companies route
-router.post("/api/leads", async (req, res) => {
-  //console.log("REQEST: ", req.headers.authorization.split(" ")[1]);
-  try {
-    const { companyId } = req.body;
-    const response = await axios.post(
-      "https://cx9dev.contegris.com:8443/apis/adb/companies/searchLeads?$top=5&$skip=0",
-      { id: "132" },
-      { headers: { Authorization: req.headers.authorization.split(" ")[1] } }
-    );
-    return res.status(200).json({
-      status: "ok",
-      data: response.data.data,
-    });
-  } catch (error) {
-    console.log(error.message);
-    return res.status(404).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
+router.post("/api/leads", async(req, res) => {
+    //console.log("REQEST: ", req.headers.authorization.split(" ")[1]);
+    try {
+        const { companyId } = req.body;
+        const response = await axios.post(
+            "https://cx9dev.contegris.com:8443/apis/adb/companies/searchLeads?$top=10&$skip=0", { id: "132" }, { headers: { Authorization: req.headers.authorization.split(" ")[1] } }
+        );
+        return res.status(200).json({
+            status: "ok",
+            data: response.data.data,
+        });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(404).json({
+            status: "fail",
+            message: error.message,
+        });
+    }
 });
 
-router.post("/api/companies", async (req, res) => {
-  try {
-    const { email, name, phone, weblink } = req.body;
-    const response = await axios.get(
-      "https://cx9dev.contegris.com:8443/apis/adb/companies/search?$top=50&$skip=0&$filter=name eq 'some Compoany'",
-      { headers: { Authorization: req.headers.authorization.split(" ")[1] } }
-    );
-    return res.status(200).json({
-      status: "ok",
-      data: response.data,
-    });
-  } catch (error) {
-    console.log(error.message);
-    return res.status(404).json({
-      status: "fail",
-      message: error.message,
-    });
-  }
+router.post("/api/companies", async(req, res) => {
+    console.log("TOKEN: ", req.headers.authorization.split(" ")[1]);
+    try {
+        const { email, name, phone, weblink } = req.body;
+        const response = await axios.post(
+            "https://cx9dev.contegris.com:8443/apis/adb/companies/search?$top=5&$skip=0&$filter=name eq 'some Compoany'", { filters: [] }, {
+                headers: { Authorization: req.headers.authorization.split(" ")[1] },
+                contentType: "application/json",
+            }
+        );
+        return res.status(200).json({
+            status: "ok",
+            data: response.data,
+        });
+    } catch (error) {
+        console.log(error.message);
+        return res.status(404).json({
+            status: "fail",
+            message: error.message,
+        });
+    }
 });
 
 module.exports = router;
